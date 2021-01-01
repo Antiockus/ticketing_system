2393521d69aec7e241c31e1df58aedc8db719c08
Tim Squires <antiockus@Tims-MacBook-Air.local>
2021-01-01 12:04:20 -0500

    added some views

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
+
+Route::get('/', function(){ return view('welcome'); });
+
 Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+
+Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
